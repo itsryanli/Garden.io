266b0a06a08d6ef8c6ebc68e261eddfbb7451d94
chunseng721 <115942038+chunseng721@users.noreply.github.com>
2024-01-14 23:03:41 +0800

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -23,7 +23,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // weather api key
   // apiKey = c5637386d600144341ebeee38e35c864, replace this with the apiKey below
-  final _weatherService = WeatherService('c5637386d600144341ebeee38e35c864');
+  final _weatherService = WeatherService('apiKey');
 
   Weather? _weather;
 
